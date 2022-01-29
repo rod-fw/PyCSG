4cc1ac25f307eeccacbd7ba975fea4c68e9f65d0
rod-fw <rod@fastwave.com.au>
2022-01-29 10:07:34 +0800

    typo

--- pycsg/operation.py
+++ pycsg/operation.py
@@ -5,9 +5,15 @@ class Operation(Shape):
     shapes: list[Shape]
     __slots__ = tuple(__annotations__)
 
-    def __ini__(self,*s):
+    def __init__(self,*s):
         self.shapes = list(s)
 
+    def add(self,shape : Shape):
+        self.shapes.append(shape)
+        return self
+        
+    __iadd__ = add
+    
 class Union(Operation):
     name = "union"
 
