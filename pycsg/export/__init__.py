0dcb8bc9068ba707703930c0917d3be2f9596f83
rod-fw <rod@fastwave.com.au>
2022-01-29 13:17:37 +0800

    ..

---
+++ pycsg/export/__init__.py
@@ -0,0 +1,11 @@
+
+def write(scene,path,protocol="openscad"):
+    if protocol == "openscad":
+        from  .openscad import OpenSCAD
+        generator = OpenSCAD()
+    else:
+        raise NotImplemented(protocol)
+        
+    fp = open(path,"w")
+    generator(scene,fp)
+    fp.close()
