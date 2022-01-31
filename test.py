755efd3263b9ec66ac7ad789eeef31a5cabd846d
rod-fw <rod@fastwave.com.au>
2022-01-31 12:35:48 +0800

    newline

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from pycsg import scene
+ 
 
 s = scene.Scene()
 s += scene.Cylinder((250,130,130)).translate((10,10,10))
@@ -6,14 +7,5 @@ s += scene.Cylinder((250,130,130)).translate((100,10,10))
 
 
 import pycsg.export
-
-pycsg.export.write(s,"test.scad")
-""" 
-fp = open("test.scad","w")
-
-from pycsg.generators import openscad
-
-gen = openscad.OpenSCAD()
-gen(s,fp)
-
-fp.close() """
+pycsg.export.write(s,"test")
+ 
