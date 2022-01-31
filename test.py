b7e4a7f90df5f9a10e4a74b284be99c4ab9a5e90
rod-fw <rod@fastwave.com.au>
2022-01-31 14:51:56 +0800

    alpha 1

--- test.py
+++ test.py
@@ -1,14 +1,30 @@
+"""
+Section	Height (mm)	Width (mm)
+Top section float cylinder - above water	75	130
+x - above water	0	0
+x - above water	0	0
+Top section float cylinder - below water	220	130
+MId section fins plate	300	130
+Bottom section payload cylinder	250	130
+Bottom section hemisphere	65	130
+x - below water	0	0
+x - below water	0	0
+Total	910	 
+
+
+"""
+
 from pycsg import scene
  
-lungo_d = 130
+lungo_r = 65
 wing_d  = 40
 wing_r  = wing_d/2
 wing_t = 5
-wing_w = (lungo_d-wing_d)/2
+wing_w = 130
 
-float_h   = 300-(lungo_d/2)
-wing_h    = 400
-payload_h = 200
+float_h   = 295
+wing_h    = 300
+payload_h = 250
 payload_d = 120
 antenna_d = 30
 
@@ -17,13 +33,18 @@ dcell_d = 33
 
 s = scene.Scene()
 # FLOAT
-s += scene.Cylinder((float_h,lungo_d,lungo_d)).translate((0,0,payload_h + wing_h))
+s += scene.Cylinder((float_h,lungo_r,lungo_r)).translate((0,0,payload_h + wing_h))
 # WING
-s += scene.Cylinder((wing_h,wing_d,wing_d)).translate((0,0,payload_h))
-
 s += scene.Union(
-    scene.Cylinder((payload_h,lungo_d, lungo_d)),
-    scene.Sphere((lungo_d,))
+    scene.Cylinder((wing_h,wing_r,wing_r)).translate((0,0,payload_h)),
+    scene.Cube((wing_w,wing_t,wing_h)).translate((-lungo_r,0,payload_h)),
+    scene.Cube((wing_w,wing_t,wing_h)).translate((0,-lungo_r,payload_h)).rotate((0,0,90)),
+)
+
+# PAYLOAD
+s += scene.Union(
+    scene.Cylinder((payload_h,lungo_r, lungo_r)),
+    scene.Sphere((lungo_r,))
 )
 import pycsg.export
 pycsg.export.write(s,"test")
--- test.scad
+++ test.scad
@@ -1,6 +1,10 @@
- translate([0,0,600])  cylinder(h=235.0,r1=130,r2=130);
- translate([0,0,200])  cylinder(h=400,r1=40,r2=40);
+ translate([0,0,550])  cylinder(h=295,r1=65,r2=65);
 union() {
- cylinder(h=200,r1=130,r2=130);
- sphere(r=130);
+ translate([0,0,250])  cylinder(h=300,r1=20.0,r2=20.0);
+ translate([-65,0,250])  cube([130,5,300]);
+ translate([0,-65,250])  rotate([0,0,90])  cube([130,5,300]);
+}
+union() {
+ cylinder(h=250,r1=65,r2=65);
+ sphere(r=65);
 }
