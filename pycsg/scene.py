37876b1ac4d86af7dac589bb454edeb376f67243
rod-fw <rod@fastwave.com.au>
2022-02-03 14:10:50 +0800

    ..

--- pycsg/scene.py
+++ pycsg/scene.py
@@ -7,15 +7,27 @@ from .transform import Translate
 from .vector import Vector
 from .operation import Union,Difference,Intersection
 
+class Camera:
+    position: tuple
+    lookat: tuple
+
+    __slots__ = tuple(__annotations__)
+
+    def __init__(self,p=(0,0,0),l=(0,0,0)):
+        self.position = Vector(p)
+        self.lookat = Vector(l)
+        
 class Scene:
     shapes: list[Shape]
     config: dict
+    camera : Camera
 
     __slots__ = tuple(__annotations__)
 
     def __init__(self) -> None:
         self.shapes = []
         self.config = {}
+        self.camera = Camera()
 
     def add(self,shape : Shape):
         self.shapes.append(shape)
