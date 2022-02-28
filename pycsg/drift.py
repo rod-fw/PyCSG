8a56fbab8eec3f6fb6f30bdcb0722fd48638caa2
rod-fw <rod@fastwave.com.au>
2022-02-28 13:43:52 +0800

    ..

--- pycsg/drift.py
+++ pycsg/drift.py
@@ -1,7 +1,4 @@
 # drag coeficient B27
- 
-
-
 cd = 0.59
 # fluid desnity B28
 fd = 1.2
@@ -9,11 +6,15 @@ fd = 1.2
 fa = 0.0001
 #velocity ms B30
 v = 2
-
 #velocity kmh B31
 v = 7.2
 
-#f = B27*(0.5*B28*(B30^2)*B29)
 
+#f = B27*(0.5*B28*(B30^2)*B29)
 def drift(cd,fd,ms,fa):
     return cd*(0.5*fd*(v*v)*fa)
+
+#Ac=B27*B29
+
+def ac(cd,fa):
+    return cd*fa
